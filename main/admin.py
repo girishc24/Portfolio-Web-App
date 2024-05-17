aff80b8349fb2865989aed96ed75c797da208648
Girish C <girishpluto@gmail.com>
2024-05-17 23:11:10 +0530

    Admin Custom AmdinPanel

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
-
+from .models import Tag, Project, ProjectImage
 # Register your models here.
+
+class ProjectImageInline(admin.TabularInline):
+    model = ProjectImage
+    extra = 1
+
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = (
+        "title",
+        "link"
+    )
+    inlines = [ProjectImageInline]
+    search_fields = ("titile", "description")
+    list_filter = ("tags", )
+
+class Tags(admin.ModelAdmin):
+    list_display = ("name",)
+    search_fields = ("name",)
+
+admin.site.register(Tag)
+admin.site.register(Project,ProjectAdmin)
+admin.site.register(ProjectImage)
--- main/models.py
+++ main/models.py
@@ -7,7 +7,7 @@ class Tag(models.Model):
     def __str__(self):
         return self.name
     
-    
+
 class Project(models.Model):
     title = models.CharField(max_length=200)
     description = models.TextField()
@@ -16,6 +16,13 @@ class Project(models.Model):
 
     def __str__(self):
         return self.title
+    
+class ProjectImage(models.Model):
+    Project = models.ForeignKey(Project, related_name="images", on_delete=models.CASCADE)
+    image = models.ImageField(upload_to="project_images/")
+
+    def __str__(self):
+        return self.Project.title
 
 
 
